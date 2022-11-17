9b66a50f0f4a135ae571a25e83fc0d748181521c
lena <ekolod5@mail.ru>
2022-11-17 13:41:17 +0300

    Программа, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго

---
+++ T08/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число, умноженное на само себя).;
+// 4 -> 16;
+// -3 -> 9;
+// -7 -> 49;
+
+Console.WriteLine("Input number");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number * number);
