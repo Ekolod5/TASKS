5259fa52710e7af8e6a3119bfa130f19b85c8c74
lena <ekolod5@mail.ru>
2022-11-17 13:51:30 +0300

    Программа, которая будет выдавать название дня недели по заданному номеру

---
+++ T13/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.;
+// 3 -> среда;
+// 5 -> пятница;
+
+Console.WriteLine("Week day!");
+string[] Days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+int index = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Today is {Days[index-1]}");
